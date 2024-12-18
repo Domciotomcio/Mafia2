e6990f12f2b4b0c8f7ee32dea5d230c422d74527
Dominik <dmk.tomaszewski@gmail.com>
2024-12-18 15:37:14 +0100

    Initial Commit

---
+++ Mafia2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mafia2
+//
+//  Created by Dominik Tomaszewski on 18/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mafia2/Mafia2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mafia2App.swift
+//  Mafia2
+//
+//  Created by Dominik Tomaszewski on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mafia2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
